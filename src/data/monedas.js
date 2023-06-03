3280aaae9ffea70c58855bff54d7e229b6248059
diego1891 <diegobronc1508@gmail.com>
2023-06-03 20:55:23 -0300

    Proyecto terminado

---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'UYU', nombre: 'Peso Uruguayo'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'ARG', nombre: 'Peso Argentino'}
+]
+export {monedas}
